92e4569110a84d42f6ad5113ee93db9e1fe974b0
Unknown <loudelat@amazon.com>
2018-05-17 12:18:21 -0700

    Final project

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using alias2a = ConsoleApp2.Program2;
+using alias2b = ConsoleApp22.Program22;
+
+class UsingDirectives
+{
+    public static void Main(string[] args)
+    {
+        alias2a.print();
+        alias2b.print();
+        ConsoleApp1.Program1.print();
+        ConsoleApp11.Program11.print();
+    }
+}
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        public static void print()
+        {
+            Console.WriteLine("This is from the ConsoleApp1 namespace Program1");
+        }
+    }
+}
+
+namespace ConsoleApp11
+ {
+     class Program11
+        {
+            public static void print()
+            {
+                Console.WriteLine("This is from the ConsoleApp11 namespace Program11");
+            }
+        }
+ }
---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using alias1a = ConsoleApp1.Program1;
+using alias1b = ConsoleApp11.Program11;
+
+namespace ConsoleApp2
+
+{
+    class Program2
+    {
+        public static void print()
+        {
+            Console.WriteLine("This is from ConsoleApp2 namespace Program2");
+        }
+    }
+}
+
+namespace ConsoleApp22
+{
+    class Program22
+    {
+        public static void print()
+        {
+            Console.WriteLine("This is from ConsoleApp22 namespace Program22");
+        }
+    }
+}
